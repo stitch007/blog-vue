184f9e4be6a45ad55824a089ad59a72ce5be3b68
Stitch <2273753879@qq.com>
2022-04-20 20:15:31 +0800

    feat: markdown code prism

--- src/composables/useMarkdown.ts
+++ src/composables/useMarkdown.ts
@@ -2,6 +2,7 @@ import MarkdownIt from 'markdown-it'
 import markdownItAnchor from 'markdown-it-anchor'
 import markdownItTocDoneRight from 'markdown-it-toc-done-right'
 import markdownItLinkAttributes from 'markdown-it-link-attributes'
+import markdownItPrism from 'markdown-it-prism'
 import type { ComputedRef } from 'vue'
 import { markdownItImageStyle } from '@/utils'
 
@@ -27,6 +28,7 @@ const md = MarkdownIt()
       target: '_blank',
     },
   })
+  .use(markdownItPrism)
   .use(markdownItImageStyle)
 
 export const useMarkdown = (markdown: ComputedRef<string | undefined>) => {
--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,6 @@ import { setupRouter } from './router'
 
 import '@unocss/reset/tailwind.css'
 import '@/assets/style/main.css'
-import '@/assets/style/markdown.css'
-import '@/assets/style/toc.css'
 import 'uno.css'
 
 const app = createApp(App)
