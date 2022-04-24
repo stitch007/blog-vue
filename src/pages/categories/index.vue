9f3b642eac92bd15950cb6cfe047f56b38a93e10
Stitch <2273753879@qq.com>
2022-04-24 18:25:02 +0800

    feat: pages /categories /tags

---
+++ src/pages/categories/index.vue
@@ -0,0 +1,92 @@
+<script setup lang="ts">
+import * as echarts from 'echarts/core'
+import type { PieSeriesOption } from 'echarts/charts'
+import { PieChart } from 'echarts/charts'
+import { LabelLayout } from 'echarts/features'
+import { CanvasRenderer } from 'echarts/renderers'
+import type { LegendComponentOption, ToolboxComponentOption } from 'echarts/components'
+import {
+  LegendComponent,
+  TitleComponent,
+  ToolboxComponent,
+  TooltipComponent,
+} from 'echarts/components'
+import { useAppStore, useLibraryStore } from '@/stores'
+
+type EChartsOption = echarts.ComposeOption<ToolboxComponentOption | LegendComponentOption | PieSeriesOption>
+
+echarts.use([
+  TitleComponent,
+  ToolboxComponent,
+  TooltipComponent,
+  LegendComponent,
+  PieChart,
+  CanvasRenderer,
+  LabelLayout,
+])
+
+const lib = useLibraryStore()
+const app = useAppStore()
+const router = useRouter()
+const chartEl = ref<HTMLElement>()
+let chart: echarts.ECharts
+
+const option = computed((): EChartsOption => {
+  return {
+    legend: app.smallerThanMd ? { top: 'bottom' } : { right: 'right' },
+    series: [
+      {
+        name: '全部分类',
+        type: 'pie',
+        radius: app.smallerThanMd ? [15, 80] : [40, 150],
+        center: app.smallerThanMd ? ['50%', '40%'] : ['50%', '50%'],
+        roseType: 'area',
+        itemStyle: {
+          borderRadius: 8,
+        },
+        label: {
+          formatter: '{b}:{c}篇({d}%)',
+        },
+        data: [
+          { name: '机器人', value: 22 },
+          { name: '知识总结', value: 2 },
+          { name: '学习笔记', value: 6 },
+          { name: '算法与数据结构', value: 17 },
+          { name: '人工智能', value: 12 },
+          { name: '嵌入式', value: 1 },
+        ],
+      },
+    ],
+  }
+})
+
+watch(lib.categories, () => {
+  chart && chart.setOption(option.value)
+})
+
+useEventListener('resize', () => {
+  chart && chart.resize()
+})
+
+onMounted(() => {
+  if (chartEl.value) {
+    chart = echarts.init(chartEl.value)
+    chart.setOption(option.value)
+    chart.on('click', (params) => {
+      console.log(params)
+      router.push(`/categories/${params.name}`)
+    })
+  }
+})
+</script>
+
+<template>
+  <main max-w-1200px m="xauto t-20" p="x2 md:x8">
+    <Card p="x8 y6">
+      <div text-2xl>
+        全部分类
+      </div>
+      <div ref="chartEl" h="80 md:100" h-100 />
+    </Card>
+  </main>
+</template>
---
+++ src/pages/tags/index.vue
@@ -0,0 +1,74 @@
+<script setup lang="ts">
+import randomColor from 'randomcolor'
+// import { useLibraryStore } from '@/stores'
+
+// const lib = useLibraryStore()
+
+const tags = [
+  { tag: { name: 'Chrome', id: 1 }, articles: [''] },
+  { tag: { name: '教程', id: 3 }, articles: ['', '', '', '', '', '', ''] },
+  { tag: { name: '二叉树', id: 4 }, articles: ['', ''] },
+  { tag: { name: '哈希表', id: 5 }, articles: ['', '', '', '', '', '', ''] },
+  { tag: { name: '前端', id: 6 }, articles: ['', '', '', '', '', '', '', '', ''] },
+  { tag: { name: '计算机图形学', id: 7 }, articles: ['', '', ''] },
+  { tag: { name: '机器学习', id: 18 }, articles: [''] },
+  { tag: { name: '编译原理', id: 8 }, articles: ['', '', '', '', '', '', '', '', '', '', '', ''] },
+  { tag: { name: 'Python', id: 10 }, articles: ['', '', '', '', '', ''] },
+  { tag: { name: 'Mybatis', id: 20 }, articles: [''] },
+  { tag: { name: '爬虫', id: 11 }, articles: ['', '', '', '', '', ''] },
+  { tag: { name: '大数据', id: 12 }, articles: ['', '', '', '', '', '', '', '', ''] },
+  { tag: { name: '数据结构', id: 14 }, articles: ['', '', '', '', '', '', '', '', ''] },
+  { tag: { name: 'CSS', id: 15 }, articles: ['', '', '', '', '', '', '', '', ''] },
+  { tag: { name: 'Spring', id: 16 }, articles: ['', '', '', '', '', ''] },
+  { tag: { name: 'Java', id: 17 }, articles: [''] },
+  { tag: { name: '正则表达式', id: 19 }, articles: [''] },
+  { tag: { name: 'C++', id: 21 }, articles: [''] },
+  { tag: { name: '数据库', id: 22 }, articles: [''] },
+  { tag: { name: 'MySql', id: 23 }, articles: [''] },
+  { tag: { name: 'IDEA', id: 24 }, articles: [''] },
+  { tag: { name: 'WebStorm', id: 25 }, articles: [''] },
+  { tag: { name: '类型体操', id: 9 }, articles: ['', '', '', '', '', '', '', '', '', ''] },
+  { tag: { name: '后端', id: 26 }, articles: [''] },
+  { tag: { name: 'iPhone', id: 27 }, articles: [''] },
+  { tag: { name: 'HTML', id: 2 }, articles: ['', '', '', '', '', '', '', '', '', '', '', '', '', ''] },
+  { tag: { name: '工作', id: 28 }, articles: [''] },
+  { tag: { name: 'Vue', id: 13 }, articles: ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''] },
+  { tag: { name: '学习', id: 29 }, articles: [''] },
+]
+
+const styles = (num: number) => {
+  return {
+    color: randomColor(),
+    fontSize: `${1.25 + num * 0.04}rem`,
+  }
+}
+</script>
+
+<template>
+  <main max-w-1200px m="xauto t-20" p="x2 md:x8">
+    <Card p="x2 y6 md:x18" text-center>
+      <div w-full text-2xl pb-4>
+        {{ '标签 - ' + tags.length }}
+      </div>
+      <div>
+        <RouterLink
+          v-for="tag in tags"
+          :key="tag.tag.id"
+          :to="`/tags/${tag.tag.name}`"
+          :style="styles(tag.articles.length)"
+          hover="!bg-$primary-color !text-gray-100 scale-108"
+          inline-block
+          m="x2 y2"
+          p="x2 md:x4"
+          bg="zinc-100 dark:zinc-900"
+          leading-loose
+          rounded-xl
+          duration-300
+        >
+          <span>{{ tag.tag.name }}</span>
+          <sup>{{ tag.articles.length }}</sup>
+        </RouterLink>
+      </div>
+    </Card>
+  </main>
+</template>
