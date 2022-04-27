1e77f25ec0d4bb030cd2b43615bf37839ec8550a
Stitch <2273753879@qq.com>
2022-04-27 13:19:36 +0800

    refactor: 修改Footer样式

--- src/components/common/Footer.vue
+++ src/components/common/Footer.vue
@@ -3,8 +3,7 @@
     flex
     justify-center
     py-6
-    text="base gray-100 dark:gray-400"
-    font-bold
+    text="sm gray-100 dark:gray-300"
     select-none
   >
     2022-PRESENT · Made by Stitch
